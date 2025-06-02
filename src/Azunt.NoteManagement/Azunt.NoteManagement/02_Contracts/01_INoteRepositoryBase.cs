6a954a3f7c7396f52f41e22d5e24342e86e2f235
Park YongJune <redplus@redplus.net>
2025-06-03 04:07:26 +0900

    Add INoteRepositoryBase and INoteRepository interfaces for Note entity with basic and extended operations

---
+++ src/Azunt.NoteManagement/Azunt.NoteManagement/02_Contracts/01_INoteRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿using Azunt.Repositories;
+
+namespace Azunt.NoteManagement;
+
+/// <summary>
+/// 기본 CRUD 작업을 위한 Note 전용 저장소 인터페이스
+/// </summary>
+public interface INoteRepositoryBase : IRepositoryBase<Note, long>
+{
+}
---
+++ src/Azunt.NoteManagement/Azunt.NoteManagement/02_Contracts/03_INoteStorageService.cs
@@ -0,0 +1,9 @@
+﻿using Azunt.Storage;
+
+namespace Azunt.NoteManagement
+{
+    public interface INoteStorageService : IStorageService
+    {
+        // Empty
+    }
+}
