dfffb47ac860b6ba6f89022cf2cf3715027c6ae5
Park YongJune <redplus@redplus.net>
2025-06-05 03:17:03 +0900

    Add EF Core DbContext and factory for Note entity with default configuration support

---
+++ src/Azunt.NoteManagement/Azunt.NoteManagement/03_Repositories/NoteDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Azunt.NoteManagement
+{
+    public class NoteDbContext : DbContext
+    {
+        public NoteDbContext(DbContextOptions<NoteDbContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>()
+                .Property(m => m.Created)
+                .HasDefaultValueSql("GetDate()");
+        }
+
+        public DbSet<Note> Notes { get; set; } = null!;
+    }
+}
